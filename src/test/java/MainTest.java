c7b8390353d538743798da6ab9559acc9a681439
alxparahin <Parakhin.A.Vla@sberbank.ru>
2019-10-18 13:52:37 +0300

    run test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,27 +2,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+/*
+    Main main;
+
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        main = new Main();
     }
 
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
+        main = null;
     }
+*/
 
+/*
     @org.junit.jupiter.api.Test
     void main() {
     }
+*/
 
     @org.junit.jupiter.api.Test
     void getMathMiddle() {
+        double[] numbers = new double[]{2d,2d,5d};
+        assertEquals(3d, Main.getMathMiddle(numbers));
     }
 
-    @org.junit.jupiter.api.Test
+/*    @org.junit.jupiter.api.Test
     void getPrimeNumbers() {
     }
 
     @org.junit.jupiter.api.Test
     void existsNumberArgument() {
     }
+*/
 }
