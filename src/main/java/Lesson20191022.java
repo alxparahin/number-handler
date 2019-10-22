e4c613d1a86710d1386978ca00f2a259ac60cf0d
alxparahin <Parakhin.A.Vla@sberbank.ru>
2019-10-22 11:38:02 +0300

    Rename 2 Lesson20191021

--- src/main/java/Lesson20191022.java
+++ src/main/java/Lesson20191022.java
@@ -7,20 +7,26 @@
 
 public class Lesson20191022 {
 
-    public static void zeroStrings(int stringsNumber){
+    public static int zeroStrings(int stringsNumber){
+        int returnNumber = 0;
         for(int i = 0; i < stringsNumber; i++){
             for(int j = 0; j <= i; j++){
+                returnNumber++;
                 System.out.print("0");
             }
             System.out.println("");
         }
+        return returnNumber;
     }
 
-    public static void englishByNumber(int number){
+    public static int englishByNumber(int number){
+        int returnNumber = 0;
         for(int i = (int)'a'; i <= (int)'z'; i++){
+            returnNumber++;
             System.out.print((char)i);
             if((i-1)%number == 0 || i == (int)'z'){System.out.println("");}
         }
+        return returnNumber;
     }
 
 }
